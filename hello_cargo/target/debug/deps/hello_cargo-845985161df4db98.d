383a82073cf81ae9d072c13e3929d83c0750a8fe
Aziz Faozi <admin@labseni.com>
2021-08-27 21:23:46 +0700

    pluma

---
+++ hello_cargo/target/debug/deps/hello_cargo-845985161df4db98.d
@@ -0,0 +1,5 @@
+/home/faoziaziz/Documents/pemrog_rust/hello_cargo/target/debug/deps/hello_cargo-845985161df4db98: src/main.rs
+
+/home/faoziaziz/Documents/pemrog_rust/hello_cargo/target/debug/deps/hello_cargo-845985161df4db98.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/faoziaziz/Documents/pemrog_rust/hello_cargo/target/debug/hello_cargo: /home/faoziaziz/Documents/pemrog_rust/hello_cargo/src/main.rs
